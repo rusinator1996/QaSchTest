e950248156f136b1cdfa2b09a58856c2ec5e0c30
Ruslan <ruslan.lihno@appflame.com>
2023-12-05 21:50:10 +0200

    Add idea to .gitignore

--- src/main/java/Currency/CurrencyConverter.java
+++ src/main/java/Currency/CurrencyConverter.java
@@ -4,23 +4,19 @@ public class CurrencyConverter {
     private double commission;
     private double exchangeRate;
 
-    // Конструктор для ініціалізації полів
     public CurrencyConverter(double commission, double exchangeRate) {
         this.commission = commission;
         this.exchangeRate = exchangeRate;
     }
 
-    // Метод для конвертації долара в гривню
     private double convertUsdToUah(double amountInUsd) {
         return amountInUsd * exchangeRate;
     }
 
-    // Метод для обрахунку комісії
     private double calculateCommission(double amountInUsd) {
         return amountInUsd * exchangeRate * commission / 100.0;
     }
 
-    // Публічний метод для обрахунку суми до виплати
     public double calculateTotalAmount(double amountInUsd) {
         double amountInUah = convertUsdToUah(amountInUsd);
         double commissionAmount = calculateCommission(amountInUsd);
